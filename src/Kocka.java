53e9ed2bf8e99b4e553332e96d0756ef4d2f1773
Smotko <114558526+Smotko@users.noreply.github.com>
2023-02-22 22:39:45 +0100

    Add files via upload

--- src/Kocka.java
+++ src/Kocka.java
@@ -1,9 +1,34 @@
+import java.util.Random;
 class Kocka{
-    String[] vrednostii = new String[6];
-    String[] vrednosti(){
-        for(int i=1; i<6; i++){
-            vrednostii[i] = "Ploskev: "+i+" Stevilo pik: "+i;
-        }
-        return vrednostii;
+    int steviloPloskev;
+
+    Kocka(int steviloPl) {
+        steviloPloskev = steviloPl;
     }
+
+    int[] poskusi = new int[steviloPloskev]; //{1, 2, 3};
+
+    void vrednosti(int steviloPloskev){
+        for(int i = 0; i < steviloPloskev; i++){
+            System.out.println
+            ("ploskev "+ i + " = stevilo pik " + i );
+        }
+    }
+    
+  /*   int[] metanje(int steviloP){
+        Random rand = new Random(steviloP);
+        for(int i = 0; i < poskusi.length; i++){
+            poskusi[i] = rand.nextInt(steviloP);
+        }
+        return poskusi;
+    }*/
+    int[] zaporedje(int poskusov) {
+        Random r = new Random();
+        poskusi = r.ints(poskusov, 0, steviloPloskev).toArray();
+        for(int i = 0; i < poskusov; i++){
+            poskusi[i]++;
+        }
+        return poskusi;
+    }
+
 }
--- src/Vaja3.java
+++ src/Vaja3.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
-import java.util.Random;
+
+//import java.util.Random;
+//import java.util.Scanner;
+
 class Vaja3{
     public static void main(String[] args){
-      Scanner sc = new Scanner(System.in);
-      Random rand = new Random();
-      Kocka k = new Kocka();
-      int ploskve = sc.nextInt();
-      System.out.println(k.vrednosti());
+    int[] a = new int[3];
+
+        Scanner sc = new Scanner(System.in);
+        int pos = sc.nextInt();
+       // Random rand = new Random();
+        a[0] = 6;
+        a[1] = 12;
+        a[2] = 20;
+         for(int i = 0; i<3; i++) {
+            Kocka k = new Kocka(a[i]);   //new Kocka(a[i], sc.nextInt() , rand.nextInt(20));
+          //  Kocka(a[i]);
+            int[] aaa = k.zaporedje(pos);
+            for(int j = 0; j < aaa.length; j++)
+                System.out.println(aaa[j]);
+        }
     }
 }
